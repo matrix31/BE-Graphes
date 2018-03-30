399ace94d1e0ae91f9aff0a7ead4f901df7205c3
M. Gaillard Paul <pgaillar@insa-10227>
2018-03-30 10:46:11 +0200

    Launch terminé.

--- src/main/org/insa/base/Launch.java
+++ src/main/org/insa/base/Launch.java
@@ -11,9 +11,7 @@ import javax.swing.SwingUtilities;
 
 import org.insa.graph.Graph;
 import org.insa.graph.Path;
-import org.insa.graph.io.BinaryGraphReader;
-import org.insa.graph.io.GraphReader;
-import org.insa.graph.io.PathReader;
+import org.insa.graph.io.*;
 import org.insa.graphics.drawing.Drawing;
 import org.insa.graphics.drawing.components.BasicDrawing;
 
@@ -54,21 +52,22 @@ public class Launch {
                 new DataInputStream(new BufferedInputStream(new FileInputStream(mapName))));
 
         // TODO: Read the graph.
-        Graph graph = null;
+        Graph graph = reader.read();
 
         // Create the drawing:
         Drawing drawing = createDrawing();
 
         // TODO: Draw the graph on the drawing.
-
+        drawing.drawGraph(graph);
         // TODO: Create a PathReader.
-        PathReader pathReader = null;
+        PathReader pathReader = new BinaryPathReader(
+                new DataInputStream(new BufferedInputStream(new FileInputStream(pathName))));
 
         // TODO: Read the path.
-        Path path = null;
+        Path path = pathReader.readPath(graph);
 
         // TODO: Draw the path.
-
+        drawing.drawPath(path);
     }
 
 }
--- src/main/org/insa/graph/Path.java
+++ src/main/org/insa/graph/Path.java
@@ -74,7 +74,6 @@ public class Path {
      * @throws IllegalArgumentException If the list of nodes is not valid, i.e. two
      *         consecutive nodes in the list are not connected in the graph.
      * 
-     * @deprecated Need to be implemented.
      */
     public static Path createShortestPathFromNodes(Graph graph, List<Node> nodes)
             throws IllegalArgumentException {
