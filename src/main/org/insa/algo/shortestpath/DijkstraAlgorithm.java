a6baf1258596d85433124b5e281f149ba0df9f68
M. Gaillard Paul <pgaillar@insa-10230>
2018-04-13 18:23:32 +0200

    Disktra OK s'arrete quand destination trouve

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -34,8 +34,12 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         labels[data.getOrigin().getId()] = new Label(data.getOrigin());
         labels[data.getOrigin().getId()].setCost(0);
         
+        //observateur origine
+        this.notifyOriginProcessed(data.getOrigin());
+        
         //Création arbre binaire
         BinaryHeap<Label> heap = new BinaryHeap<Label>();
+        
         //Premier label dans l'arbre
         heap.insert(labels[data.getOrigin().getId()]);
         
@@ -43,19 +47,31 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         int mark = 0;
         Label labelx;
         Node noeudx;
-        
+        boolean found = false;
         //Algo de dijkstra
-        while(mark < heap.size()) {
+        while(mark < heap.size() && !found ) {
         	//On recup le premier element de l'arbre et on met sa mark à 1
         	labelx = heap.deleteMin();
         	labelx.setMark(1);
+        	if(labelx.getNoeud() == data.getDestination()) {
+        		found = true;
+             	// The destination has been found, notify the observers.
+         		notifyDestinationReached(data.getDestination());
+        	}
+	        //observateur neud marker
+	        this.notifyNodeMarked(labelx.getNoeud());
+	        
         	//On récupère ses noeuds adjacents et on boucle sur les arcs
         	noeudx = labelx.getNoeud();
         	for(Arc arc : noeudx) {
+        		
         		//Si le label de l'arc n'existe pas on l'initialise
         		if(labels[arc.getDestination().getId()] == null) {
         			labels[arc.getDestination().getId()] = new Label(arc.getDestination());
+        	        //observateur 1er passage
+        	        this.notifyNodeReached(arc.getDestination());
         		}
+        		
         		//Si coût actuel du noeud > au cout nouveau on l'actualise et si l'arc est autorisé au mode de transport
         		if(labels[arc.getDestination().getId()].getCost() > labelx.getCost() + arc.getLength() && data.isAllowed(arc)) {
         			labels[arc.getDestination().getId()].setCost(labelx.getCost() + arc.getLength());
@@ -73,13 +89,12 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         	}
         }
         ShortestPathSolution solution = null;
+        
         // Destination has no predecessor, the solution is infeasible...
      	if (labels[data.getDestination().getId()] == null) {
      		solution = new ShortestPathSolution(data, Status.INFEASIBLE);
      	} else {
 
-     	// The destination has been found, notify the observers.
-     		notifyDestinationReached(data.getDestination());
      	// Create the path from the array of nodes...
      		ArrayList<Node> nodes = new ArrayList<>();
      		//On crée un tableau de nodes en récupant tous les father
